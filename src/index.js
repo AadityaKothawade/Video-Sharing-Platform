71c3f1168c2337e917d0722bc63d7f4bfcd6b2c3
aaditya <aadityakothawade72@gmail.com>
2025-09-10 17:57:09 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is listening at PORT ${process.env.PORT} `)
+    })
+})
+.catch((err)=>{
+    console.log("MONDB connection failed !!! ",err)
+})
 
 
 
